71893f7a223da6d308813d2e25112b63fd82e4e0
max-gallagher <70865313+max-gallagher@users.noreply.github.com>
2023-02-05 18:42:17 -0500

    Update dns-resolver.ps1

--- dns-resolver.ps1
+++ dns-resolver.ps1
@@ -1,6 +1,6 @@
 param ($network, $server)
 
-for ($i = 0; $i -le 256; $i++) 
+for ($i = 0; $i -le 255; $i++) 
 {
     $ip = ($network + "." + $i)
     $hostname = (Resolve-DnsName -DnsOnly $ip -Server $server -ErrorAction Ignore)
